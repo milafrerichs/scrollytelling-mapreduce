8485e4f214e9704429a985cf113fdc900ea9e114
Mila Frerichs <mila.frerichs@gmail.com>
2022-03-09 12:52:49 +0100

    chore: add colors to tailwind config and update game

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./node_modules/map-reduce-game/**/*.svelte",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'app-teal': {
+          300:'#C5EAF5',
+          400:'#C5EAE3',
+          500:'#66A2AD',
+        },
+        'question-heading': '#624D4D',
+      }
+    }
   },
   plugins: [],
 }
